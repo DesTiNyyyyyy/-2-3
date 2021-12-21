44701ccf15efd1402060da92e63342d5246eac54
Всеславинский Александр Александрович <is119vseslavinskiy@oat.local>
2021-12-21 13:01:06 +0600

    commit name

---
+++ сессия3/сессия3/Window1.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Web;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace сессия3
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void bt1_click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
